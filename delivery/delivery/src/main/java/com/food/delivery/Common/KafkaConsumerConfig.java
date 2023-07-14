507eee0535d73202d444ad1986b89f52b3653f3f
TharshiEY <ey.tharshi@gmail.com>
2023-07-14 11:22:20 +0530

    first commit with project

---
+++ delivery/delivery/src/main/java/com/food/delivery/Common/KafkaConsumerConfig.java
@@ -0,0 +1,30 @@
+package com.food.delivery.Common;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KafkaConsumerConfig {
+//    @Value("${kafka.bootstrap.servers}")
+//    private String bootstrapServers;
+
+//    @Value("${kafka.consumer.group-id}")
+//    private String groupId;
+
+    @Bean
+    public KafkaConsumer<Integer, String> kafkaConsumer() {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, AppConfigs.groupID);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new KafkaConsumer<>(properties);
+    }
+}
